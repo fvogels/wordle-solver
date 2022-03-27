1b9fd3c80c4eb1ca6b106306b34c6b333c079dca
Frederic Vogels <frederic.vogels@ucll.be>
2022-03-27 17:17:16 +0200

    cpp

--- cpp/cpp/app.cpp
+++ cpp/cpp/app.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include <cstdint>
+#include <atomic>
+#include <thread>
 #include <memory>
 #include <chrono>
 #include <string>
@@ -27,6 +29,18 @@ std::string to_word(wordid id)
     }
 
     return std::string(buffer);
+}
+
+wordid from_word(const std::string& string)
+{
+    wordid word = 0;
+
+    for (size_t i = 0; i != string.size(); ++i)
+    {
+        word = word * 26 + string[i] - 'a';
+    }
+
+    return word;
 }
 
 std::string to_score(scoreid id)
@@ -92,7 +106,6 @@ public:
     }
 };
 
-
 Scorer load_scorer(const std::string& path)
 {
     Scorer scorer;
@@ -123,6 +136,22 @@ Scorer load_scorer(const std::string& path)
     }
 
     return scorer;
+}
+
+template<typename T>
+std::vector<wordid> filter_words(const Scorer& scorer, const T& words, wordid guess, scoreid score)
+{
+    std::vector<wordid> result;
+
+    for (auto word : words)
+    {
+        if (scorer.lookup(word, guess) == score)
+        {
+            result.push_back(word);
+        }
+    }
+
+    return result;
 }
 
 template<typename T>
@@ -162,7 +191,11 @@ double measure_information_gained(const Scorer& scorer, scoreid score, wordid gu
         information -= incompatible_count * log2(incompatible_count);
     }
 
-    return information / total;
+    information /= total;
+
+    // std::cout << to_word(guess) << " " << compatible_count << " " << incompatible_count << ": " << information << " bits" << std::endl;
+
+    return information;
 }
 
 template<typename T>
@@ -187,12 +220,16 @@ double average_information_gained(const Scorer& scorer, wordid guess, const T& c
         count++;
     }
 
-    return total_information / count;
+    double average = total_information / count;
+
+    // std::cout << "Average for " << to_word(guess) << ": " << average << std::endl;
+    
+    return average;
 }
 
 
-template<typename T>
-wordid find_best_guess(const Scorer& scorer, const T& candidate_solutions, const T& candidate_guesses)
+template<typename T, typename U>
+wordid find_best_guess(const Scorer& scorer, const T& candidate_solutions, const U& candidate_guesses)
 {
     double most_information_gained = 0;
     wordid best_guess = 0;
@@ -225,11 +262,118 @@ wordid find_best_guess(const Scorer& scorer, const T& candidate_solutions, const
     return best_guess;
 }
 
+template<typename T, typename U>
+wordid find_best_guess(const Scorer& scorer, const T& candidate_solutions, const U& candidate_guesses, unsigned nthreads)
+{
+    auto most_information_gained = std::make_unique<double[]>(nthreads);
+    auto best_guess = std::make_unique<wordid[]>(nthreads);
+    auto start_time = std::chrono::steady_clock::now();
+    std::atomic<unsigned> progress(0);
+    unsigned progress_goal = 0;
+
+    std::vector<std::vector<wordid>> batches(nthreads);
+    std::vector<std::thread> threads;
+
+    {
+        unsigned i = 0;
+
+        for (auto& guess : candidate_guesses)
+        {
+            batches[i].push_back(guess);
+            i = (i + 1) % nthreads;
+            progress_goal++;
+        }
+    }
+
+    for (unsigned i = 0; i != nthreads; ++i)
+    {
+        auto mig = &most_information_gained[i];
+        auto bg = &best_guess[i];
+        auto batch = &batches[i];
+        *mig = 0;
+
+        std::thread thread([mig, bg, batch, &scorer, &candidate_guesses, &progress]() {
+            for (auto guess : *batch)
+            {
+                auto information = average_information_gained(scorer, guess, candidate_guesses);
+
+                if (information > *mig)
+                {
+                    *mig = information;
+                    *bg = guess;
+                }
+
+                ++progress;
+            }
+        });
+
+        threads.push_back(std::move(thread));
+    }
+
+    {
+        auto start_time = std::chrono::steady_clock::now();
+        
+        while (progress < progress_goal)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+            auto current_time = std::chrono::steady_clock::now();
+            auto time_passed = std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time);
+            auto progress_percentage = double(progress) / candidate_guesses.size();
+            auto estimated_duration = time_passed / progress_percentage;
+            auto time_left = estimated_duration - time_passed;
+
+            std::cout << (progress_percentage * 100) << "% done in " << time_passed.count() << "s; estimated total duration: " << (estimated_duration.count() / 60) << "m, " << (time_left.count() / 60) << "m left" << std::endl;            
+        }
+    }
+
+    for (auto& thread : threads)
+    {
+        thread.join();
+    }
+    
+    double mig = 0;
+    wordid bg = 0;
+
+    for (unsigned i = 0; i != nthreads; ++i)
+    {
+        if (most_information_gained[i] > mig)
+        {
+            mig = most_information_gained[i];
+            bg = best_guess[i];
+        }
+    }
+
+    return bg;
+}
+
 int main()
 {
+    // jazzy
     auto scorer = load_scorer(R"(G:\repos\wordle\scores.compressed)");
     auto& words = scorer.wordids();
-    auto best_guess = find_best_guess(scorer, words, words);
 
+    //{
+    //    auto word = from_word("jazzy");
+    //    auto information = average_information_gained(scorer, word, words);
+    //    std::cout << to_word(word) << std::endl;
+    //    std::cout << information << std::endl;
+    //}
+
+    //{
+    //    auto word = from_word("steak");
+    //    auto information = average_information_gained(scorer, word, words);
+    //    std::cout << to_word(word) << std::endl;
+    //    std::cout << information << std::endl;
+    //}
+
+
+    //auto best_guess = find_best_guess(scorer, words, words);
+    //std::cout << to_word(best_guess) << std::endl;
+
+
+    auto sel = filter_words(scorer, words, from_word("jazzy"), 0);
+    std::cout << sel.size() << std::endl;
+    auto best_guess = find_best_guess(scorer, sel, words, 6);
     std::cout << to_word(best_guess) << std::endl;
 }
