54674d2723ddfd3f692daf4536da873f303bef85
Frederic Vogels <frederic.vogels@ucll.be>
2022-03-28 09:27:21 +0200

    refactored ruby solver

--- ruby/wordle.rb
+++ ruby/wordle.rb
@@ -1,11 +1,91 @@
-# SCORES_FILENAME = 'scores.txt'
-SCORES_FILENAME = 'scores.bin'
-# SCORES_FILENAME = 'scores-light.bin'
-# SCORES_FILENAME = 'testwords.bin'
-# SCORES_FILENAME = 'scores-light.txt'
+require 'optparse'
 
-def unparse_score(n)
-  n.to_s(3).rjust(5, '0').chars.map do |c|
+
+class Judge
+  def average_information_gained(guess:, candidate_solutions:)
+    candidate_solutions.group_by do |candidate_solution|
+      s = judge(solution: candidate_solution, guess: guess)
+    end.map do |key, words|
+      words.size * Math.log2(words.size)
+    end.sum.then do |information|
+      Math.log2(candidate_solutions.size) - information / candidate_solutions.size
+    end
+  end
+
+
+  def find_best_guess(candidate_solutions:, candidate_guesses:)
+    candidate_guesses.max_by do |guess|
+      average_information_gained(guess: guess, candidate_solutions: candidate_solutions)
+    end
+  end
+end
+
+
+class CachedJudge < Judge
+  def initialize(filename)
+    @table = Marshal.load(IO.binread(filename))
+  end
+
+  def judge(solution:, guess:)
+    @table["#{solution}:#{guess}"]
+  end
+
+  def words
+    @words ||= @table.keys.map { |k| k.split(':')[0] }.uniq
+  end
+end
+
+
+class StandardJudge < Judge
+  def judge(solution:, guess:)
+    solution = solution.chars
+    guess = guess.chars
+    freqs = solution.tally
+    result = [ :wrong ] * solution.size
+
+    (0...solution.size).zip(solution, guess).each do |index, expected, actual|
+      if expected == actual
+        result[index] = :correct
+        freqs[expected] -= 1
+        if freqs[expected] == 0
+          freqs.delete expected
+        end
+      end
+    end
+
+    (0...solution.size).each do |index|
+      if freqs.has_key?(guess[index]) && result[index] == :wrong
+        result[index] = :misplaced
+        freqs[guess[index]] -= 1
+        if freqs[guess[index]] == 0
+          freqs.delete guess[index]
+        end
+      end
+    end
+
+    result
+  end
+end
+
+
+def judgement_id(judgement)
+  judgement.map do |x|
+    case x
+    when :correct
+      2
+    when :misplaced
+      1
+    when :wrong
+      0
+    end
+  end.reduce(0) do |acc, n|
+    acc * 3 + n
+  end
+end
+
+
+def unparse_judgement(id)
+  id.to_s(3).rjust(5, '0').chars.map do |c|
     case c
     when '0'
       'W'
@@ -17,72 +97,38 @@ def unparse_score(n)
   end.join
 end
 
-def load_score_table(filename, mode)
-  case mode
-  when :binary
-    Marshal.load(IO.binread(filename))
 
-  when :text
-    abort "Not supported yet"
-  end
-  # table = Hash.new do |h, k|
-  #   h[k] = {}
-  # end
+def parse_command_line_arguments
+  options = { judge: StandardJudge.new }
 
-  # IO.readlines(SCORES_FILENAME).each do |line|
-  #   solution, guess, score = line.strip.split
+  OptionParser.new do |opts|
+    opts.on('-c [FILENAME]', 'Load cached judge (no word list necessary)') do |filename|
+      judge = CachedJudge.new(filename)
+      options[:judge] = judge
+      options[:words] = judge.words
+    end
 
-  #   table[solution][guess] = score.to_i
-  # end
+    opts.on('-w [FILENAME]', 'Use word list') do |filename|
+      options[:words] = IO.readlines(filename).map(&:strip)
+    end
+  end.parse!
 
-  # table
-end
-
-STDERR.puts "Loading scores..."
-$score_table = load_score_table(SCORES_FILENAME, :binary)
-$dictionary = $score_table.keys.map { |k| k.split(':')[0] }.uniq
-STDERR.puts "Done!"
-
-def score(solution:, guess:)
-  $score_table["#{solution}:#{guess}"]
-end
-
-
-def average_information_gained(guess:, candidate_solutions:)
-  candidate_solutions.group_by do |candidate_solution|
-    s = score(solution: candidate_solution, guess: guess)
-  end.map do |key, words|
-    words.size * Math.log2(words.size)
-  end.sum.then do |information|
-    Math.log2(candidate_solutions.size) - information / candidate_solutions.size
-  end
-end
-
-
-def find_best_guess(candidate_solutions:, candidate_guesses:)
-  candidate_guesses.max_by do |guess|
-    average_information_gained(guess: guess, candidate_solutions: candidate_solutions)
-  end
+  options
 end
 
 
 def main
-  best_guess = find_best_guess(candidate_solutions: $dictionary, candidate_guesses: $dictionary)
+  options = parse_command_line_arguments
+
+  judge = options[:judge]
+  words = options[:words]
+
+  abort "ERROR: No judge" unless judge
+  abort "ERROR: No words" unless words
+
+  best_guess = judge.find_best_guess(candidate_solutions: words, candidate_guesses: words)
   puts "Best guess: #{best_guess}"
 end
 
-def test
-  dictionary = $dictionary
 
-  # dictionary.each do |guess|
-  #   s = score(solution: 'stake', guess: guess)
-  #   p information_gained(guess_score: s, guess: guess, candidate_solutions: dictionary)
-  # end
-
-  puts find_best_guess(candidate_solutions: dictionary, candidate_guesses: dictionary)
-end
-
-main
-
-# main if $0 == __FILE__
-# test if $0 == __FILE__
+main if $0 == __FILE__
